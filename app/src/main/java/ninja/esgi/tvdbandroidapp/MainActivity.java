a9e9c274f18296d05c2906c714f08bd9f588006c
Roland Kullet <renaud.bellec.3@gmail.com>
2017-11-21 18:34:58 +0100

    Init app

---
+++ app/src/main/java/ninja/esgi/tvdbandroidapp/MainActivity.java
@@ -0,0 +1,13 @@
+package ninja.esgi.tvdbandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
