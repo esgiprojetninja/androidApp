92fb0888911ac15be824745026d7abdd02ed252b
dydyFos <dylanfoster.esgi@gmail.com>
2017-12-27 14:58:06 +0100

    login activity with back btn

--- app/src/main/java/ninja/esgi/tvdbandroidapp/MainActivity.java
+++ app/src/main/java/ninja/esgi/tvdbandroidapp/MainActivity.java
@@ -1,8 +1,11 @@
 package ninja.esgi.tvdbandroidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-        getSupportActionBar().hide();
     }
+
+    public void loginActivity(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void logout(View view) {
+        // @TODO clear creadentials, kill session, redirect to mainactivity
+        Log.d("logout", "hovering out");
+    }
+
 }
