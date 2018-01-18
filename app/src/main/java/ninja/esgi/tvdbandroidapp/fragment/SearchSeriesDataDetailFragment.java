6664c12d3235b130aeed84067bd348188ffd3bcb
dydyFos <dylanfoster.esgi@gmail.com>
2018-01-18 01:24:04 +0100

    bb comments, next step: ratings on series

--- app/src/main/java/ninja/esgi/tvdbandroidapp/fragment/SearchSeriesDataDetailFragment.java
+++ app/src/main/java/ninja/esgi/tvdbandroidapp/fragment/SearchSeriesDataDetailFragment.java
@@ -67,7 +67,6 @@ public class SearchSeriesDataDetailFragment extends DialogFragment {
         this.fetchData();
 
         // @TODO GET series/{id}/actors
-        // @TODO GET series/{id}/episodes
         // @TODO GET episodes/{id} on click ?
         // @TODO GET /user/ratings
         // @TODO PUT /user/ratings/{itemType}/{itemId}/{itemRating}
@@ -85,10 +84,6 @@ public class SearchSeriesDataDetailFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // The only reason you might override this method when using onCreateView() is
-        // to modify any dialog characteristics. For example, the dialog includes a
-        // title by default, but your custom layout might not need it. So here you can
-        // remove the dialog title, but you must call the superclass to get the Dialog.
         Dialog dialog = super.onCreateDialog(savedInstanceState);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         return dialog;
